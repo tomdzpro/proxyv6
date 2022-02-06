8b665768ead533c986771e48321222014753887d
tomdzpro <80185659+tomdzpro@users.noreply.github.com>
2022-02-06 23:18:58 +0700

    Update installv2.sh

--- installv2.sh
+++ installv2.sh
@@ -13,9 +13,9 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/tomdzpro/proxyv6/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://raw.githubusercontent.com/tomdzpro/proxyv6/main/3proxy-0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
