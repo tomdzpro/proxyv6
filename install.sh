ea2a8cfab4eb4c4ff2cda452b2904b95e015aaef
tomdzpro <80185659+tomdzpro@users.noreply.github.com>
2022-02-07 07:32:08 +0700

    Change Port
    Change Port

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 echo "How many proxy do you want to create? Example 500"
 read COUNT
 
-FIRST_PORT=10000
+FIRST_PORT=14000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
